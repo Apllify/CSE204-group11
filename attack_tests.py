b3c85f82586c8dd0d1da144532a0725a3ee7ce7e
Apllify <ralilahlou50@gmail.com>
2023-05-26 17:55:23 +0200

    spoofer function

--- attack_tests.py
+++ attack_tests.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-import img_manipulations
+from img_manipulations import *
 import random
 
 def attack_tests(sample_list, attack_func, database_x, database_y, cnn_model, pca_model, dnn_model):
@@ -20,7 +20,14 @@ def attack_tests(sample_list, attack_func, database_x, database_y, cnn_model, pc
 
 
 
-def generate_spoofed_dataset(dataset):
+def generate_spoofed_dataset(database_x, database_y):
+    """
+    Generates a list of same length and labels as x but with random filter functions
+    applied to each element
+
+    database_y should NOT be categorical, just the regular database_y
+    
+    """
     spoofed_dataset = np.array([])
 
     #distributions of filters (relative values)
@@ -31,11 +38,58 @@ def generate_spoofed_dataset(dataset):
     perlin_noise_odd = 1
     flip_image_odd = 1
 
+    #quirks of filter that need them
+    rotation_angle = 20
+    gaussian_blur_sigma = 1
+    box_blur_kernel = 2
+    uniform_max_noise = 0.3
+    perlin_max_noise = 0.3
+
     total = rotation_odd + gaussian_blur_odd + box_blur_odd + \
             uniform_noise_odd + perlin_noise_odd + flip_image_odd
 
-    for image in dataset:
+    for i, image in np.ndenumerate(dataset_x):
         rand = random.random() * total
+
+        if dataset_y[i] not in (6, 9): #avoid rotating the numbers 6 and 9 
+
+            if rand <= rotation_odd/total : 
+                np.append(spoofed_dataset, rotate_image(image, rotation_angle))
+
+            elif rand <= (rotation_odd + gaussian_blur_odd)/total:
+                np.append(spoofed_dataset, gaussian_blur(image, gaussian_blur_sigma))
+
+            elif random <= (rotation_odd + gaussian_blur_odd + box_blur_odd)/total:
+                np.append(spoofed_dataset, box_blur(image, box_blur_kernel) )
+
+            elif random <= (rotation_odd + gaussian_blur_odd + box_blur_odd + uniform_noise_odd)/total:
+                np.append(spoofed_dataset, uniform_noise(image, uniform_max_noise))
+
+            elif random <= (rotation_odd + gaussian_blur_odd + box_blur_odd + uniform_noise_odd + perlin_noise_odd)/total:
+                np.append(spoofed_dataset, perlin_noise(image, perlin_max_noise))
+
+            else:
+                np.append(spoofed_dataset, flip_image(image))
+
+
+        else:
+            if rand <= (rotation_odd + gaussian_blur_odd)/total:
+                np.append(spoofed_dataset, gaussian_blur(image, gaussian_blur_sigma))
+
+            elif random <= (rotation_odd + gaussian_blur_odd + box_blur_odd)/total:
+                np.append(spoofed_dataset, box_blur(image, box_blur_kernel) )
+
+            elif random <= (rotation_odd + gaussian_blur_odd + box_blur_odd + uniform_noise_odd)/total:
+                np.append(spoofed_dataset, uniform_noise(image, uniform_max_noise))
+
+            elif random <= (rotation_odd + gaussian_blur_odd + box_blur_odd + uniform_noise_odd + perlin_noise_odd)/total:
+                np.append(spoofed_dataset, perlin_noise(image, perlin_max_noise))
+
+            else:
+                np.append(spoofed_dataset, flip_image(image))
+
+
+    return spoofed_dataset
 
 # Visualizing blur 
 
