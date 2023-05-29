dec71269da1feeb4710f1fb80d79546f3e275fef
Luigi Massacci <luigi.massacci.it@gmail.com>
2023-05-29 15:27:54 +0200

    cross attack lattice tests

--- main.py
+++ main.py
@@ -36,12 +36,19 @@ ry_train_cat = utils.to_categorical(ry_train, 10)
 ry_test_cat = utils.to_categorical(ry_test, 10)
 
 
-pca_model = PCA_model(250, 10)
+# pca_model = PCA_model(250, 10)
 
-lattice = attack_lattice(PCA_model, (x_train, y_train_cat), (x_test, y_test_cat), gaussian_blur_database, np.arange(0, 2, 0.1))
+# lattice = attack_lattice(PCA_model, (x_train, y_train_cat), (x_test, y_test_cat), gaussian_blur_database, np.arange(0, 2, 0.1))
 
-np.savetxt("pca_gauss_lattice.txt", lattice)
+# np.savetxt("pca_gauss_lattice.txt", lattice)
 
+lattice =np.loadtxt('pca_gauss_lattice.txt')
+
+plt.pcolormesh(np.arange(0, 2, 0.1), np.arange(0, 2, 0.1), lattice.T, cmap='RdBu')
+plt.xlabel("Training dataset max blur")
+plt.ylabel("Testing dataset max blur")
+plt.title("PCA model with Gaussian Blur")
+plt.show()
 
 
 """ PCA Model Training Code
@@ -62,41 +69,41 @@ cnn_model.save_weights('cnn_weights')
 """
 
 
-#loading the models from memory
-pca_model = PCA_model.load("pca_weights") 
-dnn_model = PCA_model.load("dnn_weights") 
+# #loading the models from memory
+# pca_model = PCA_model.load("pca_weights") 
+# dnn_model = PCA_model.load("dnn_weights") 
 
-cnn_model = CNN_model()
-cnn_model.load_weights('cnn_weights')
-
-
-
-#BOILERPLATE code for generating and plotting the effect of an attack
-n_samples = 8
-attack_function = perlin_noise_database
-
-arguments = np.array(  [[i/10] for i in range(8)] )
-# arguments[:, 0] = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5]
-# arguments[:, 1] = arguments[:, 0]
-
-#x_axis = arguments[:, 0]
-x_axis = [i/10 for i in range(8)]
-
-model_list = [pca_model, dnn_model, cnn_model]
+# cnn_model = CNN_model()
+# cnn_model.load_weights('cnn_weights')
 
 
-result = run_attacks(x_test, y_test_cat, model_list, attack_function, arguments)
+
+# #BOILERPLATE code for generating and plotting the effect of an attack
+# n_samples = 8
+# attack_function = perlin_noise_database
+
+# arguments = np.array(  [[i/10] for i in range(8)] )
+# # arguments[:, 0] = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5]
+# # arguments[:, 1] = arguments[:, 0]
+
+# #x_axis = arguments[:, 0]
+# x_axis = [i/10 for i in range(8)]
+
+# model_list = [pca_model, dnn_model, cnn_model]
 
 
-plt.plot(x_axis, result[0], label="PCA Model")
-plt.plot(x_axis, result[1], label="DNN Model")
-plt.plot(x_axis, result[2], label="CNN Model")
-plt.legend(loc="upper right")
-plt.xlabel("Maximum perlin noise level allowed (1 = 100%)")
-plt.ylabel("Accuracy of models")
+# result = run_attacks(x_test, y_test_cat, model_list, attack_function, arguments)
 
 
-plt.show()
+# plt.plot(x_axis, result[0], label="PCA Model")
+# plt.plot(x_axis, result[1], label="DNN Model")
+# plt.plot(x_axis, result[2], label="CNN Model")
+# plt.legend(loc="upper right")
+# plt.xlabel("Maximum perlin noise level allowed (1 = 100%)")
+# plt.ylabel("Accuracy of models")
+
+
+# plt.show()
 
 
 
